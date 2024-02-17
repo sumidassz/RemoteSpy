ac43a31a4eafdcbff8986c0d8804d01d380e51e2
sumidassz <89941722+sumidassz@users.noreply.github.com>
2024-02-17 18:47:36 +0000

    Update testa.lua

--- testa.lua
+++ testa.lua
@@ -1,36 +1,10 @@
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/sumidassz/RemoteSpy/main/RemoteSpyMenu.lua"))()
-function createHideButton(gui)
-    local HideButton = Instance.new("ImageButton", gui.GUI)
-    local UICorner = Instance.new("UICorner", HideButton)
-    HideButton.Name = "HideButton"
-    HideButton.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
-    HideButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
-    HideButton.BorderSizePixel = 0
-    HideButton.Position = UDim2.new(0.00833767373, 0, 0.107970171, 0)
-    HideButton.Selectable = false
-    HideButton.Size = UDim2.new(0.0534201749, 0, 0.0980202928, 0)
-    HideButton.AutoButtonColor = false
-    HideButton.MouseButton1Up:Connect(function()
-        gui.MainFrame.Visible = not gui.MainFrame.Visible
-    end)
-end
-local Window = Material.Load({Title = "Remote Spy by Miojo Hub", Style = 2, SizeX = 400, SizeY = 350, Theme = "Aqua"})
-local Options = Material.Options
-local Main = Window.New({Title = "Remote Spy"})
-createHideButton(Material)
+--[[
 
-Main.DataTable({
-	Text = "DataTable RemoteSpy",
-    Id = "Main",
-	Callback = function(ChipSet)
-	end,
-	Options = {
-        ["Remote Spy"] = {Enabled = false, Id = "remoteSpy"},
-        ["Copy Remote"] = {Enabled = false, Id = "copyRemote"}
-	}
-})
+	-Created by Vaeb.
 
-_G.scanRemotes = false
+]]
+
+_G.scanRemotes = true
 
 _G.ignoreNames = {
 	Event = true;
@@ -91,7 +65,7 @@ local function parseData(obj, numTabs, isKey, overflow, noTables, forceDict)
 		end
 
 		if isDict or hasTables or forceDict then
-			out[#out+1] = (isCyclic and "Cyclic " or "") .. "{"
+			out[#out+1] = "{"
 			table.sort(data, function(a, b)
 				local aType = typeof(a[2])
 				local bType = typeof(b[2])
@@ -115,7 +89,7 @@ local function parseData(obj, numTabs, isKey, overflow, noTables, forceDict)
 					local nowValType = typeof(nowVal)
 					out[#out+1] = parseKey .. " = " .. parseVal .. ";"
 				else
-					out[#out+1] = parseVal .. ";"
+					out[#out+1] =  parseVal .. ";"
 				end
 			end
 			out[#out+1] = "}"
@@ -201,9 +175,9 @@ local incId = 0
 local function getValues(self, key, ...)
 	return {realMethods[key](self, ...)}
 end
-local copyingRemote = false
+
 gameMeta.__index, gameMeta.__namecall = function(self, key)
-	if not realMethods[key] or _G.ignoreNames[self.Name] or not Options.Main.remoteSpy.Value or Material.isClosed then return pseudoEnv.__index(self, key) end
+	if not realMethods[key] or _G.ignoreNames[self.Name] or not _G.scanRemotes then return pseudoEnv.__index(self, key) end
 	return function(_, ...)
 		incId = incId + 1
 		local nowId = incId
@@ -217,7 +191,6 @@ gameMeta.__index, gameMeta.__namecall = function(self, key)
 		if ok then
 			returnValues = data
 			print("game."..self:GetFullName()..":"..key..tableToString(allPassed))
-            if Options.Main.copyRemote.Value then setclipboard("game."..self:GetFullName()..":"..key..tableToString(allPassed)) end
 		else
 			print("\n" .. strId .. " ClassName: " .. self.ClassName .. " | Path: " .. self:GetFullName() .. " | Method: " .. key .. "\n" .. strId .. " Packed Arguments: " .. tableToString(allPassed) .. "\n" .. strId .. " Packed Returned: [ERROR] " .. data .. "\n")
 		end
@@ -225,4 +198,5 @@ gameMeta.__index, gameMeta.__namecall = function(self, key)
 		return unpack(returnValues)
 	end
 end
-print("\nRemoteSpy\n")
+
+print("\nRan Vaeb's RemoteSpy\n")
